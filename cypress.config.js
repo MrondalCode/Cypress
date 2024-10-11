e2992d0426a3ce54c8aebd99abfb25031272e9de
Michael Rondal <alexandj2015@gmail.com>
2024-10-11 08:50:18 -0500

    ConfiguracionReportes

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     pageLoadTimeout: 30000, // 120 segundos
     setupNodeEvents(on, config) {
       // implementar los event listeners aquí si es necesario
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 };
